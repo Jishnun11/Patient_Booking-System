1fd29279d4aacf732a23e9eabe17a5fd591808aa
Jishnun11 <jishnun@decasync.com>
2026-06-10 11:55:44 +0530

    first commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to Inertia + React!</h1>
+            <p>Your React frontend is now integrated with Laravel.</p>
+        </div>
+    );
+}
